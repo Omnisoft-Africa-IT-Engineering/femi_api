a4ff37939f146318f4ffba491eb01eefc715fad1
emile <emileahaliga11@gmail.com>
2026-09-21 15:12:56 +0000

    Ajout de endpoints pour creation de compte avec google

--- apps/femi_account/urls.py
+++ apps/femi_account/urls.py
@@ -4,17 +4,23 @@ from rest_framework.routers import DefaultRouter
 from .views import (
     RegisterView,
     PublicLoginView,
-    EcheanceFiscaleViewSet
+    GoogleLoginView,
+    CreateEntrepriseView,
+    EcheanceFiscaleViewSet,  # <--- Importé depuis views.py
 )
 
 router = DefaultRouter()
 router.register(r'echeances-fiscales', EcheanceFiscaleViewSet, basename='echeancefiscale')
 
 urlpatterns = [
-    # Endpoints Auth
+    # Authentification classique & OAuth
     path('public/signup/', RegisterView.as_view(), name='public-signup'),
     path('public/login/', PublicLoginView.as_view(), name='public-login'),
+    path('public/google-login/', GoogleLoginView.as_view(), name='public-google-login'),
 
-    # Endpoints CRUD Échéances Fiscales
+    # Configuration PME
+    path('entreprise/setup/', CreateEntrepriseView.as_view(), name='entreprise-setup'),
+
+    # Router Échéances Fiscales OTR
     path('', include(router.urls)),
 ]
